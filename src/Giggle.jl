bfecb7003ddcbb265ff9f79ea5d7c22956010317
kreikrei <kreitonstrs@gmail.com>
2021-01-17 20:04:48 +0700

    terakhir bet ngerevise buildMaster amin

--- src/Giggle.jl
+++ src/Giggle.jl
@@ -287,7 +287,7 @@ function buildMaster(n::node;silent::Bool)
     begin
         @objective(mp, Min,
 
-        sum(( n.base.K[k].vard * g(R[r].p[:,k,t]) + sum(deli[i,k] * R[r].v[i,k,t] for i in n.base.K[k].cover) + sum(K[k].fix * R[r].z[i,k,t] for i in n.base.K[k].cover) ) * θ[r,k,t] for r in keys(R),k in keys(n.base.K),t in n.base.T) +
+        sum(( n.base.K[k].vard * g(R[r].p[:,k,t]) + sum(n.base.deli[i,k] * R[r].v[i,k,t] for i in n.base.K[k].cover) + sum(n.base.K[k].fix * R[r].z[i,k,t] for i in n.base.K[k].cover) ) * θ[r,k,t] for r in keys(R),k in keys(n.base.K),t in n.base.T) +
         sum(n.base.V[i].h * I[i,t] for i in keys(n.base.V),t in n.base.T) +
         sum(n.stblzr.slackCoeff * slack_I[i,t] for i in keys(n.base.V),t in n.base.T) -
         sum(n.stblzr.surpCoeff * surp_I[i,t] for i in keys(n.base.V),t in n.base.T)

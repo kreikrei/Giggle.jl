f3e17d27e4fdf0ea71947493d992a9f38bb96da3
kreikrei <kreitonstrs@gmail.com>
2021-01-17 19:36:29 +0700

    add buildMaster function

--- src/Giggle.jl
+++ src/Giggle.jl
@@ -211,9 +211,10 @@ function base(path::String)
     return (K=K,V=V,T=T,d=d,dist=dist,G=G,deli=deli)
 end
 
-function initialize(dt::NamedTuple,valueSlackCoeff::Float64,valueSurplusCoeff::Float64)
-    slackCoeff = valueSlackCoeff
-    surpCoeff = valueSurplusCoeff
+#function to initialize stabilizer
+function initStab(dt::NamedTuple,vSlCoeff::Float64,vSurpCoeff::Float64)
+    slackCoeff = vSlCoeff
+    surpCoeff = vSurpCoeff
     slackLim = JuMP.Containers.DenseAxisArray{Float64}(undef,keys(dt.V),dt.T)
     surpLim = JuMP.Containers.DenseAxisArray{Float64}(undef,keys(dt.V),dt.T)
     for i in keys(dt.V),t in dt.T
@@ -225,7 +226,7 @@ function initialize(dt::NamedTuple,valueSlackCoeff::Float64,valueSurplusCoeff::F
 end
 
 #function to create root node
-function root(dt::NamedTuple;slCoeff,suCoeff)
+function root(dt::NamedTuple;slCoeff::Float64,suCoeff::Float64)
     id = uuid1(rng)
     root = node(
         id,id,
@@ -238,6 +239,64 @@ function root(dt::NamedTuple;slCoeff,suCoeff)
     return root
 end
 
-export base,root
+#function to update stabilizer
+function updateStab(stabilizers::stabilizer,param::Float64)
+    for i in first(stabilizers.slackLim.axes),t in last(stabilizers.slackLim.axes)
+        stabilizers.slackLim[i,t] = param * stabilizers.slackLim[i,t]
+        if stabilizers.slackLim[i,t] < 1
+            stabilizers.slackLim[i,t] = 0
+        end
+    end
+
+    for i in first(stabilizers.surpLim.axes),t in last(stabilizers.surpLim.axes)
+        stabilizers.surpLim[i,t] = param * stabilizers.surpLim[i,t]
+        if stabilizers.surpLim[i,t] < 1
+            stabilizers.surpLim[i,t] = 0
+        end
+    end
+
+    return stabilizers
+end
+
+function buildMaster(n::node;silent::Bool)
+    #COLUMN LABELING
+    R = Dict(1:length(n.columns) .=> n.columns)
+
+    #MODEL DECLARATION
+    mp = Model(optimizer_with_attributes(() -> Gurobi.Optimizer(GUROBI_ENV)))
+    if silent
+        set_silent(mp)
+    end
+
+    #VARIABLE DECLARATION
+    θ = @variable(mp, θ[keys(R),keys(n.base.K),T] >= 0) #LABEL 20
+    I = @variable(mp, I[keys(n.base.V),vcat(first(n.base.T)-1,n.base.T)]) #NO BOUNDS (bounded below)
+
+    #SLACK SURPLUS DECLARATION
+    @variable(mp, 0 <= slack_I[i=keys(n.base.V),t=n.base.T] <= n.stblzr.slackLim[i,t])
+    @variable(mp, 0 <= surp_I[i=keys(n.base.V),t=n.base.T] <= n.stblzr.surpLim[i,t])
+
+    #CONSTRAINT 17 & 18 + STARTING INVENTORY
+    @constraint(mp, λ[i=keys(n.base.V),t=n.base.T], I[i,t-1] + sum(R[r].q[i,k,t] * θ[r,k,t] for r in keys(R),k in keys(n.base.K)) + slack_I[i,t] - surp_I[i,t] == d[i,t] + I[i,t])
+    @constraint(mp, [i=keys(n.base.V),t=n.base.T], V[i].MIN <= I[i,t] <= V[i].MAX)
+    @constraint(mp, [i=keys(n.base.V)], I[i,first(n.base.T)-1] == V[i].START)
+
+    #CONVEXITY CONSTRAINT LABEL 19
+    @constraint(mp, δ[k=keys(n.base.K),t=n.base.T], sum(θ[r,k,t] for r in keys(R)) <= n.base.K[k].freq)
+
+    #OBJECTIVE FUNCTION
+    begin
+        @objective(mp, Min,
+
+        sum(( n.base.K[k].vard * g(R[r].p[:,k,t]) + sum(deli[i,k] * R[r].v[i,k,t] for i in n.base.K[k].cover) + sum(K[k].fix * R[r].z[i,k,t] for i in n.base.K[k].cover) ) * θ[r,k,t] for r in keys(R),k in keys(n.base.K),t in n.base.T) +
+        sum(V[i].h * I[i,t] for i in keys(n.base.V),t in n.base.T) +
+        sum(n.stblzr.slackCoeff * slack_I[i,t] for i in keys(n.base.V),t in n.base.T) -
+        sum(n.stblzr.surpCoeff * surp_I[i,t] for i in keys(n.base.V),t in n.base.T)
+        )
+    end
+end
+
+
+export base,root,buildMaster
 
 end
